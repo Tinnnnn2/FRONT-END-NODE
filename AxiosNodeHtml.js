cd023db02331a348c9f5cec527bf159135f64b0b
Tinnnnn2 <minitinvv@gmail.com>
2024-02-12 09:12:33 +0700

    asd

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 var bodyParser = require('body-parser');
 
-const base_url = "http://localhost:3000";
+const base_url = "http://localhost:10.104.12.188";
 
 app.set('view engine','ejs');
 app.use(bodyParser.json());
